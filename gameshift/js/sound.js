6b110e916461cc8a77483c76a88711d32297dbaa
GengZhengLin <gzl8042@163.com>
2014-07-16 14:07:16 +0800

    gameshift

---
+++ gameshift/js/sound.js
@@ -0,0 +1,6 @@
+var playsound=function(id){
+	var v = document.getElementById(id);
+	v.load();
+	v.play();	
+}
+//$('body').append($('<audio>').attr('id','m').attr('src','res/sound/MoveFinish.wav'));var v = document.getElementById('m');
